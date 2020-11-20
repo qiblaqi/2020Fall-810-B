5adfb0703fd65f36096ab79983a884bd5b11fc39
Qi Zhao <qizhao@Qis-Mac-mini.local>
2020-11-19 20:04:17 -0500

    Nov 19 M1 update

--- test.py
+++ test.py
@@ -1,16 +1,1 @@
-from typing import Generator
-def fool(start, end):
-    total = 0
-    for item in range(start, end):
-        total += item
-    return total
-
-assert fool(1,1)==0
-
-def echo_round() -> Generator[int, float, str]:
-    sent = yield 0
-    while sent >= 0:
-        sent = yield round(sent)
-    return 'Done'
-for index in range(3):
-    print(echo_round())
+print("hello! This is M1. ")
